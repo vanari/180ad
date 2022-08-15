b018175f4d2ccc3eaa3c107ebae5331dc52abe97
Richard Váňa <vanarius.10@gmail.com>
2022-08-15 23:52:12 +0000

    Basic nodeless setup.

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+const provinces = Array.prototype.slice.call(document.getElementsByClassName('province'));
+
+
+provinces.forEach(element => {
+    element.onclick = (e) => {
+        provinces.forEach(el => {
+            //el.style.fill = 'maroon';
+            el.classList.remove("selected");
+            el.classList.add("province");
+        })
+        //element.style.fill = 'pink';
+        element.classList.add("selected");
+        element.classList.remove("province");
+        console.log(element, element.className);
+    }
+});
+
+const header = document.getElementById('header');
+
+header.onclick = (e) => {
+    if (!(header.classList.contains("vanished"))) {
+        header.classList.add("vanished");
+    } else {
+        header.classList.remove("vanished");
+    }
+}
